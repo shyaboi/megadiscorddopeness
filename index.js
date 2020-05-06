a5ae1be3179cdab7bf06e6c304156fd1a9a4ff83
Shyaboi <ian.shane.sears@gmail.com>
2020-05-06 01:31:11 -0700

    working with author link. Almost ready to deploy on dingus crew server

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
-const token = "NzA3NDUwMTk3NDQxMjQ5MzAy.XrJgzg.LAUs10XrRcowAoRkrSa7KY3e4C4";
+const token = "NzA3NDUwMTk3NDQxMjQ5MzAy.XrJjHA.-lqa3C_WbIp7C5B0BpwTro2cvRE";
 const Discord = require("discord.js");
 const { Client, MessageAttachment, MessageEmbed } = require("discord.js");
 
@@ -12,8 +12,10 @@ client.on("ready", () => {
     .setImage("https://i.ibb.co/q0WZ7KP/download.jpg")
     .setTitle("RoButt Online! ( ͡° ͜ʖ ͡°)")
     .setDescription("Hi y'all. Operational and ready to kill")
-    .setColor(0xff0000);
-  client.channels.cache.find((x) => x.name === "test").send(embed);
+    .setColor(0xff0000)
+    .setAuthor('Shyaboi', "https://i.ibb.co/CsPSXmJ/ha.png", 'https://ianss.dev')
+
+  client.channels.cache.find((x) => x.name === "general").send(embed);
 });
 
 // client.on("message", (msg) => {
@@ -22,7 +24,7 @@ client.on("ready", () => {
 
 // Fs in the chat
 client.on("message", (message) => {
-  if (message.content === "f") {
+  if (message.content == "f") {
     // Create the attachment using MessageAttachment
     const attachment = new MessageAttachment(
       "https://pressftopayrespects.org/img/sharetw.png"
@@ -31,13 +33,18 @@ client.on("message", (message) => {
     message.channel.send(attachment);
   }
 });
--client.on("message", (message) => {
-   let time = Math.floor(Math.random()* 10000);
-    //   once a day milli 86400000
+client.on("message", (message) => {
+  let time = Math.floor(Math.random() * 864000);
+  //  a day milli                     86400000
+  console.log(time)
   setInterval(() => {
+    const meme = new MessageAttachment("https://i.kym-cdn.com/photos/images/newsfeed/001/168/682/c2f.jpg");
+    // Send the attachment in the message channel
+    message.channel.send(meme);
+    console.log(meme)
     message.channel.send("Shyaboi!");
-    console.log('msg sent')
-    console.log(time)
-  }, time)
+    console.log("msg sent");
+    console.log(time);
+  }, time);
 });
 client.login(token);
